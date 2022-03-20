bcbdc9d52ce0df55aa9633bf17c071f21da1c4c9
Sergej Stojanovic <sergej2stojanovic@gmail.com>
2022-03-20 22:55:13 +0100

    Dialogs
    Changed section into dialogs and made them work only on Enters.

--- src/js/sign.js
+++ src/js/sign.js
@@ -1,5 +1,5 @@
 const SIGN__IN__BUTTON = document.querySelectorAll('.button--sign-in')
-const SIGN__IN = document.querySelector('.sign-in')
+const SIGN__IN = document.querySelector('#sign-in')
 const SIGN__IN__CLOSE = document.querySelector('.sign-in--close')
 const CONTINUE__BUTTON = document.querySelector('.continue--button--sign-in')
 
@@ -9,11 +9,13 @@ function HideSignIn() {
     if (hiddenSignIn) {
         SIGN__IN.classList.add('slide--right')
         SIGN__IN.classList.remove('slide-out')
+        SIGN__IN.show()
 
         hiddenSignIn = false
     } else if (!hiddenSignIn) {
         SIGN__IN.classList.remove('slide--right')
         SIGN__IN.classList.add('slide-out')
+        SIGN__IN.close()
 
         hiddenSignIn = true
     }
@@ -23,19 +25,25 @@ for (let i = 0; i < SIGN__IN__BUTTON.length; i++) {
     SIGN__IN__BUTTON[i].addEventListener('click', () => {
         HideSignIn()
     })
-    SIGN__IN__BUTTON[i].addEventListener('keydown', () => {
-        HideSignIn()
+    SIGN__IN__BUTTON[i].addEventListener('keydown', e => {
+        if (e.key == 'enter') {
+            HideSignIn()
+        }
     })
 }
 SIGN__IN__CLOSE.addEventListener('click', () => {
     HideSignIn()
 })
-SIGN__IN__CLOSE.addEventListener('keydown', () => {
-    HideSignIn()
+SIGN__IN__CLOSE.addEventListener('keydown', e => {
+    if (e.key == 'enter') {
+        HideSignIn()
+    }
 })
 CONTINUE__BUTTON.addEventListener('click', () => {
     HideSignIn()
 })
-CONTINUE__BUTTON.addEventListener('keydown', () => {
-    HideSignIn()
+CONTINUE__BUTTON.addEventListener('keydown', e => {
+    if (e.key == 'enter') {
+        HideSignIn()
+    }
 })
--- src/js/wallet.js
+++ src/js/wallet.js
@@ -1,5 +1,5 @@
 const SAVING__BUTTON = document.querySelector('.saving__button')
-const WALLET = document.querySelector('.wallet')
+const WALLET = document.querySelector('#wallet')
 const WALLET__CLOSE = document.querySelector('.wallet__close')
 const CONTINUE__BUTTON = document.querySelector('.continue--button--wallet')
 
@@ -9,11 +9,13 @@ function HideWallet() {
     if (hiddenWallet) {
         WALLET.classList.add('slide--right')
         WALLET.classList.remove('slide-out')
+        WALLET.show()
 
         hiddenWallet = false
     } else if (!hiddenWallet) {
         WALLET.classList.remove('slide--right')
         WALLET.classList.add('slide-out')
+        WALLET.close()
 
         hiddenWallet = true
     }
@@ -22,18 +24,24 @@ function HideWallet() {
 SAVING__BUTTON.addEventListener('click', () => {
     HideWallet()
 })
-SAVING__BUTTON.addEventListener('keydown', () => {
-    HideWallet()
+SAVING__BUTTON.addEventListener('keydown', e => {
+    if (e.ket == 'enter') {
+        HideWallet()
+    }
 })
 WALLET__CLOSE.addEventListener('click', () => {
     HideWallet()
 })
-WALLET__CLOSE.addEventListener('keydown', () => {
-    HideWallet()
+WALLET__CLOSE.addEventListener('keydown', e => {
+    if (e.ket == 'enter') {
+        HideWallet()
+    }
 })
 CONTINUE__BUTTON.addEventListener('click', () => {
     HideWallet()
 })
-CONTINUE__BUTTON.addEventListener('keydown', () => {
-    HideWallet()
+CONTINUE__BUTTON.addEventListener('keydown', e => {
+    if (e.ket == 'enter') {
+        HideWallet()
+    }
 })
