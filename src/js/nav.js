4c83c3a2e7d8738b929f1b51b8ac3708e7632dd7
Sergej Stojanovic <sergej2stojanovic@gmail.com>
2022-03-23 22:56:22 +0100

    Nav Switch
    Added functionality to nav switch.

--- src/js/nav.js
+++ src/js/nav.js
@@ -1,6 +1,21 @@
+const NAV__SWITCH__BACKGROUND = document.querySelector(
+    '.nav__switch--background'
+)
 const NAV__SWITCH = document.querySelector('.nav__switch')
 const HEADER = document.querySelector('.header')
 
-let open = true
+let open = false
 
-NAV__SWITCH.addEventListener('click', {})
+NAV__SWITCH__BACKGROUND.addEventListener('click', () => {
+    if (open) {
+        HEADER.classList.remove('open')
+        NAV__SWITCH.classList.remove('change')
+        NAV__SWITCH__BACKGROUND.classList.remove('change--background')
+        open = !open
+    } else if (!open) {
+        HEADER.classList.add('open')
+        NAV__SWITCH.classList.add('change')
+        NAV__SWITCH__BACKGROUND.classList.add('change--background')
+        open = !open
+    }
+})
