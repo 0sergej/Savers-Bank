72003feaa75fb73601e8434ddfecc2fa8fa06905
Sergej Stojanovic <sergej2stojanovic@gmail.com>
2022-03-26 21:56:50 +0100

    Header
    Header and nav fixes.

--- src/js/nav.js
+++ src/js/nav.js
@@ -5,8 +5,12 @@ const NAV__SWITCH = document.querySelector('.nav__switch')
 const HEADER = document.querySelector('.header')
 const NAV = document.querySelector('.nav__list')
 const NAV__ITEM = document.querySelectorAll('.nav__link')
+const FAVICON = document.querySelector('.favicon')
+const SIGN__IN__BUTTON = document.querySelector('.button--sign-in')
 
 let open = false
+let mobile = 600
+let PC = 1281
 
 function Open() {
     if (open) {
@@ -14,14 +18,22 @@ function Open() {
         NAV__SWITCH.classList.remove('change')
         NAV__SWITCH__BACKGROUND.classList.remove('change--background')
         NAV.classList.add('hidden')
+        FAVICON.classList.remove('hidden')
+        SIGN__IN__BUTTON.classList.remove('padding--reset')
 
         open = !open
     } else if (!open) {
-        HEADER.classList.add('open')
-        NAV__SWITCH.classList.add('change')
-        NAV__SWITCH__BACKGROUND.classList.add('change--background')
-        NAV.classList.remove('hidden')
-        open = !open
+        if (document.body.clientWidth < PC) {
+            HEADER.classList.add('open')
+            NAV__SWITCH.classList.add('change')
+            NAV__SWITCH__BACKGROUND.classList.add('change--background')
+            NAV.classList.remove('hidden')
+            if (document.body.clientWidth <= mobile) {
+                FAVICON.classList.add('hidden')
+                SIGN__IN__BUTTON.classList.add('padding--reset')
+            }
+            open = !open
+        }
     }
 }
 
